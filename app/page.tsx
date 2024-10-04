dc16e70227ee78193e6448600b2fdb81f82aaab0
Emilio Barrientos <emilio.barrientos@tessituranetwork.com>
2024-10-03 23:02:55 -0600

    First version

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import VideoScroll from '@/components/VideoScroll';
-
 export default function Index() {
-  return (
-    <div>
-      <VideoScroll />
-    </div>
-  );
+  return <div>Home!</div>;
 }
