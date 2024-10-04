63fe9f8b91c5a971e8189c37764db7850531eef6
Emilio Barrientos <emilio.barrientos@tessituranetwork.com>
2024-10-03 19:10:44 -0600

    fixed maps button

--- components/MapButton.tsx
+++ components/MapButton.tsx
@@ -22,7 +22,7 @@ export default function MapButton() {
 
   return (
     <button
-      className={`${playfair.className} z-10 bg-zinc-900 text-white pl-12 pr-12 py-2 flex text-sm justify-center items-center`}
+      className={`${playfair.className} z-10 bg-zinc-900 text-white pl-12 pr-11 py-2 flex text-sm justify-center items-center`}
       onClick={() => goToLocation()}
     >
       Abrir Mapa
