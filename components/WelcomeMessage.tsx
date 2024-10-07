329916216ff69e7298f4c8dabcaf160e27e0cc1a
Emilio Barrientos <emilio.barrientos@tessituranetwork.com>
2024-10-07 14:25:13 -0600

    remove effect from welcome message

--- components/WelcomeMessage.tsx
+++ components/WelcomeMessage.tsx
@@ -1,21 +1,9 @@
-import { BlurScrollEffect } from '@/utils/BlurScrollEffect';
-import { useGSAP } from '@gsap/react';
 import { Playfair_Display } from 'next/font/google';
-import React, { useRef } from 'react';
+import React from 'react';
 
 const playfair = Playfair_Display({ subsets: [] });
 
 export default function WelcomeMessage() {
-  const welcome = useRef<HTMLDivElement>(null);
-
-  useGSAP(() => {
-    if (!welcome.current) {
-      return;
-    }
-
-    new BlurScrollEffect(welcome.current, { start: 'top bottom-=40%' });
-  });
-
   return (
     <>
       <h2 className="font-lejour-serif w-full text-left text-5xl">
@@ -24,7 +12,7 @@ export default function WelcomeMessage() {
       <h2 className="-mt-4 mx-10 font-lejour-script w-full text-left text-xl opacity-25">
         Los invitamos a celebrar con nosotros
       </h2>
-      <p className={`mt-10 ${playfair.className}`} ref={welcome}>
+      <p className={`mt-10 ${playfair.className}`}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
