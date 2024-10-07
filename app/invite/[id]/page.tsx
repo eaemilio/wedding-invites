c1bff0743c1fdfddbf877fa304ff09ae86d741d6
Emilio Barrientos <emilio.barrientos@tessituranetwork.com>
2024-10-07 15:04:19 -0600

    add smoth scrolling

--- app/invite/[id]/page.tsx
+++ app/invite/[id]/page.tsx
@@ -42,7 +42,7 @@ export default function Index() {
     const video = videoRef.current;
     const container = containerRef.current;
 
-    // initSmoothScrolling();
+    initSmoothScrolling();
 
     if (!video) {
       return;
