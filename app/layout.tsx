541b570dc64f58fe0ff6a026e41b9b9e5f49f185
Emilio Barrientos <emilio.barrientos@tessituranetwork.com>
2024-10-23 11:05:45 -0600

    edited metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'Emilio and Yeraldy',
-  description: 'Nuestra invitación soñada para un evento soñado',
+  description:
+    'Únete a nuestra celebración especial. Descubre todos los detalles de nuestra boda e ingresa con tu código personalizado para confirmar tu asistencia. ¡Te esperamos con mucha ilusión!',
 };
 export default function RootLayout({
   children,
